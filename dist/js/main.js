12468f3a6bc6b8ebe066369e3515bd7b33d2949e
HuDa Qeshta <Qeshta.w.huda@gmail.com>
2020-11-15 22:54:22 +0400

    Menu button, main javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item"); //All will put all elements with nav-item class in a 'Node list' which is similar to array.
+
+// Set Initial State Of Menu
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    //Reset Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    //Reset Menu State
+    showMenu = false;
+  }
+}
